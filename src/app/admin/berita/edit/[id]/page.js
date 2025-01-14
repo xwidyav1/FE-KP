75c2ac35a92774d9540425aac08ff2a03e6732d6
Aryo Bismo Nugroho <aryobismo23@gmail.com>
2025-01-14 10:03:36 +0700

    delete all admin

--- src/app/admin/berita/edit/[id]/page.js
+++
@@ -1,9 +0,0 @@
-import Edit from "@/components/admin/berita/edit/Edit";
-
-export default function berita() {
-  return (
-    <div className="relative">
-      <Edit />
-    </div>
-  )
-}
--- src/app/admin/galeri/edit/[id]/page.js
+++
@@ -1,9 +0,0 @@
-import EditGaleri from "@/components/admin/galeri/edit/EditGaleri";
-
-export default function galeriEdit() {
-  return (
-    <div className="relative">
-      <EditGaleri />
-    </div>
-  )
-}
