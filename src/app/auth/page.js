1790ec5297b2bbffef0ece9eb822cbae85a557e2
Aryo Bismo Nugroho <aryobismo23@gmail.com>
2025-01-15 09:45:19 +0700

    feat: delete url auth

--- src/app/auth/page.js
+++
@@ -1,9 +0,0 @@
-import Login from "@/components/admin/auth/Login";
-
-export default function Home() {
-  return (
-    <div className="relative h-fit overflow-hidden">
-      <Login />
-    </div>
-  );
-}
